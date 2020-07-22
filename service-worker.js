a092f8c2531a24f7f39c65d2fc0ed9c0aaae5bc7
Zoe Tretiakova <trettttiakpva@gmail.com>
2020-07-22 11:29:26 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trettttiakova/flags-for-savva/precache-manifest.84d7d3e2215cdf1ac21079ab7a77a2cb.js"
+  "/flags-for-savva/precache-manifest.754e22307ebaa9110a5f0d6f9e75b541.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trettttiakova/flags-for-savva/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flags-for-savva/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
