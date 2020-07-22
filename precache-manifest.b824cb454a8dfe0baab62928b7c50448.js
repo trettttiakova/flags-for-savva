5f9e32586d3a927d4459bc68afd64618844e9318
Zoe Tretiakova <trettttiakpva@gmail.com>
2020-07-22 11:17:31 +0400

    Updates

---
+++ precache-manifest.b824cb454a8dfe0baab62928b7c50448.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3580d62c821886876148b642202e662",
+    "url": "/trettttiakova/flags-for-savva/index.html"
+  },
+  {
+    "revision": "53d611e3b2c804fe1e29",
+    "url": "/trettttiakova/flags-for-savva/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db3912c7eb8b81cfccdf",
+    "url": "/trettttiakova/flags-for-savva/static/js/2.3adeca41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trettttiakova/flags-for-savva/static/js/2.3adeca41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53d611e3b2c804fe1e29",
+    "url": "/trettttiakova/flags-for-savva/static/js/main.97e30beb.chunk.js"
+  },
+  {
+    "revision": "d9bab39a3393088e37c1",
+    "url": "/trettttiakova/flags-for-savva/static/js/runtime-main.00c18dac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trettttiakova/flags-for-savva/static/media/logo.5d5d9eef.svg"
+  }
+]);
